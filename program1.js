78fc2aa63f7c35918a410870a6015c6a8853ce56
Chanchal Sahu <130583371+chanchalsahu@users.noreply.github.com>
2024-02-23 12:48:52 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,11 +1,29 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function(s) {
-    
-};
 
 module.exports = { isValid };
+
+var isValid = function(s) {
+    const stack = [];
+    const brackets = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+    
+    for (const char of s) {
+        if (brackets[char]) {
+            stack.push(char);
+        } else {
+            const lastOpenBracket = stack.pop();
+            if (!lastOpenBracket || brackets[lastOpenBracket] !== char) {
+                return false;
+            }
+        }
+    }
+    
+    return stack.length === 0;
+};
+console.log(isValid("()")); // Output: true
+console.log(isValid("()[]{}")); // Output: true
+console.log(isValid("(]")); // Output: false
 
 
